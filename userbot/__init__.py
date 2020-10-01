4ea98826dec71fc4da0c4a223c1538fe782f9d8f
xonelinobot <72151700+xonelinobot@users.noreply.github.com>
2020-10-01 05:26:07 +0000

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -48,7 +48,7 @@ if version_info[0] < 3 or version_info[1] < 8:
 # Check if the config was edited by using the already used variable.
 # Basically, its the 'virginity check' for the config file ;)
 CONFIG_CHECK = (os.environ.get(
-    "___________PLOX_______REMOVE_____THIS_____LINE__________") or None)
+    "") or None)
 
 if CONFIG_CHECK:
     LOGS.info(
@@ -57,8 +57,8 @@ if CONFIG_CHECK:
     quit(1)
 
 # Telegram App KEY and HASH
-API_KEY = os.environ.get("API_KEY") or None
-API_HASH = os.environ.get("API_HASH") or None
+API_KEY = os.environ.get("API_KEY") 1861241*)
+API_HASH = os.environ.get("API_HASH") e84813247dd9743f2333e81f6a4ad051*)
 
 
 # Userbot Session String
